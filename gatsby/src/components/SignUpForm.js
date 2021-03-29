dbacfab233e7e3f496cf6585900343b2894a9977
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-03-29 08:33:35 -0400

    Correct spelling of classnames import

--- gatsby/src/components/SignUpForm.js
+++ gatsby/src/components/SignUpForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 const ErrorMessage = ({ message }) => {
   return (
