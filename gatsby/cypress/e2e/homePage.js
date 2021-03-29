e03ad0e4017364bc2601e0ecf19cb9597ec26355
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-03-29 13:35:01 -0400

    test: :white_check_mark: Update subscribe tests for removal of 'name' attribute
    #15

--- gatsby/cypress/e2e/homePage.js
+++ gatsby/cypress/e2e/homePage.js
@@ -1,7 +1,6 @@
 describe('home page', () => {
   it('shows hero image', () => {
     cy.visit('/');
-    cy.contains('Live Life').should('be.visible');
-    // cy.get('.h-full > picture > img').should('be.visible');
+    cy.get('.h-full > picture > img').should('be.visible');
   });
 });
