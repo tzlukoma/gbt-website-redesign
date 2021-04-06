7656feec40a196dd604d9963d42af88085a230fa
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-04-06 19:32:48 -0400

    Adds time to event card

--- gatsby/src/components/EventCard.tsx
+++ gatsby/src/components/EventCard.tsx
@@ -11,7 +11,10 @@ function EventCard({ event }: Props) {
   return (
     <div className="p-8  bg-light-100 border-dashed border-4 border-grey-100 shadow-md">
       <div className="pb-2 text-lg font-bold font-display">{displayLocalTimeZone(event.node.date, 'ddd')}</div>
-      <div className="pb-2 text-4xl font-bold font-display">{displayLocalTimeZone(event.node.date, 'DD MMM')}</div>
+      <div className="pb-2 text-4xl font-bold font-display">
+        <span>{displayLocalTimeZone(event.node.date, 'D MMM')}</span><span className="font-light font-serif text-primary-200">{`  |`}</span>
+        <span className="ml-3 text-2xl text-primary-400">{displayLocalTimeZone(event.node.date, 'h:mm a')}</span>
+      </div>
       <h3 className="mt-0 text-2xl">{event.node.title}</h3>
       <div className="mb-5">
         <p className="text-lg text-primary-600">{event.node?.synopsis && event.node?.synopsis[0].children[0].text}</p>
