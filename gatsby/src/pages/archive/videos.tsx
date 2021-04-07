8129433c9e807960367e7ff77ae025d0bab79006
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-04-06 22:26:17 -0400

    Code clean-up and dependency upgrades

--- gatsby/src/pages/archive/videos.tsx
+++ gatsby/src/pages/archive/videos.tsx
@@ -43,7 +43,7 @@ function videos({ data }) {
                                             videoUrl,
                                         } = event.node;
                                         return (
-                                            <li>
+                                            <li key={id}>
                                                 <article className="mb-3 overflow-hidden shadow-lg">
                                                     <div className="relative h-0 pb-fluid-video">
 
@@ -62,8 +62,8 @@ function videos({ data }) {
                                                                         width="24"
                                                                         className="m-auto mt-20"
                                                                         stroke="currentColor">
-                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                                     </svg>
                                                                 </div>
                                                             </div>
@@ -147,6 +147,7 @@ function videos({ data }) {
                                                 <div className="relative h-0 pb-fluid-video">
                                                     <ReactPlayer
                                                         className="absolute top-0 left-0 w-full h-full"
+                                                        controls={true}
                                                         width="100%"
                                                         height="100%"
                                                         loading="lazy"
