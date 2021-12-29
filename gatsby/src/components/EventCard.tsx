7a2846ab4da56316ddb3e4df0cc62dff9607bb34
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-12-28 19:42:36 -0500

    Fix failing build

--- gatsby/src/components/EventCard.tsx
+++ gatsby/src/components/EventCard.tsx
@@ -10,17 +10,17 @@ interface Props {
 function EventCard({ event }: Props) {
   return (
     <div className="p-8  bg-light-100 border-dashed border-4 border-grey-100 shadow-md">
-      <div className="pb-2 text-lg font-bold font-display">{displayLocalTimeZone(event.node.date, 'ddd')}</div>
+      <div className="pb-2 text-lg font-bold font-display">{displayLocalTimeZone(event?.node?.date, 'ddd')}</div>
       <div className="pb-2 text-4xl font-bold font-display">
-        <span>{displayLocalTimeZone(event.node.date, 'D MMM')}</span><span className="font-light font-serif text-primary-200">{`  |`}</span>
+        <span>{displayLocalTimeZone(event?.node?.date, 'D MMM')}</span><span className="font-light font-serif text-primary-200">{`  |`}</span>
         <span className="ml-3 text-2xl text-primary-400">{displayLocalTimeZone(event.node.date, 'h:mm a')}</span>
       </div>
-      <h3 className="mt-0 text-2xl">{event.node.title}</h3>
+      <h3 className="mt-0 text-2xl">{event?.node?.title}</h3>
       <div className="mb-5">
         <p className="text-lg text-primary-600">{event.node?.synopsis && event.node?.synopsis[0].children[0].text}</p>
       </div>
       {
-        event.node.registerUrl && (<button className="mb-5 px-5 py-3 rounded bg-primary-700 hover:bg-primary-100 text-white"><a href={event.node.registerUrl}>Register Here</a></button>)
+        event?.node?.registerUrl && (<button className="mb-5 px-5 py-3 rounded bg-primary-700 hover:bg-primary-100 text-white"><a href={event?.node?.registerUrl}>Register Here</a></button>)
       }
 
       <div className="col-start-1 col-span-2">
