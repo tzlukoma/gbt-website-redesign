1ab769c6b9779dafcec3b157798f3057f72cfdaf
Thomas Z Lukoma <tzlukoma@gmail.com>
2022-03-28 21:12:55 -0400

    add LogRocket

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import LogRocket from 'logrocket';
 import Layout from './src/components/Layout';
 import './src/styles/global.css';
+
+LogRocket.init('3hr770/georgebthompsoncom');
 
 export function wrapPageElement({ element, props }) {
   const queryClient = new QueryClient();
