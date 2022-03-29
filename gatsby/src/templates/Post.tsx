1f41425aff6d2728da62c7f2d4ce80e396704359
Thomas Z Lukoma <tzlukoma@gmail.com>
2022-03-28 22:32:07 -0400

    Migrate blog posts from wordpress to Sanity #9: add date to blog post

--- gatsby/src/templates/Post.tsx
+++ gatsby/src/templates/Post.tsx
@@ -1,17 +1,21 @@
 import React from "react"
 import { graphql } from 'gatsby'
 import BlockContent from '@sanity/block-content-to-react'
+import { displayLocalTimeZone } from "../utils/timeFormats"
 
 export default function SinglePostPage({ data }) {
 
+    console.log(data)
     const content = data.post._rawBody
     const categories = data.post.category
     const author = data?.post?.author?.name
     const title = data.post.title
+    const date = data.post.publishedAt
     return (
         <div className="m-auto p-8 md:px-8 lg:px-20">
             <h1 className="text-4xl text-center">{title}</h1>
-            <h2 className="text-2xl text-center">{`by ${author}`}</h2>
+            <h2 className="text-2xl text-center my-0">{displayLocalTimeZone(date, 'MMM DD, YYYY')}</h2>
+            <h2 className="text-2xl text-center mt-0">{`by ${author}`}</h2>
             <BlockContent blocks={content} className="prose lg:prose-xl m-auto" />
         </div>)
 }
@@ -29,6 +33,7 @@ export const query = graphql`
                 category {
                     name
                 }
+                publishedAt
                 _rawBody(resolveReferences: {maxDepth: 10})
             }
         }
