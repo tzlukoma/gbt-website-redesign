7b3fcde26674ac93ccb31532e638e987f0cc6d60
Thomas Z Lukoma <tzlukoma@gmail.com>
2022-05-11 21:57:40 -0400

    #48-feat: add pages for 5 levels

---
+++ gatsby/src/pages/my-level/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MyLevelPage = () => {
+    return (
+        <div className="m-auto w-1/2">
+            <h2>The Five Levels of Financial Being Quiz</h2>
+            <a href="https://my.forms.app/form/6251a6efe911ef58a523b9b8">
+                <button
+                    className="'flex-shrink-0 bg-primary-500 hover:bg-primary-600 border-primary-500 rounded hover:border-primary-600 lg:text-lg border-4 text-white py-1 px-5 lg:px-10': true,
+                    'opacity-50 cursor-not-allowed'"
+                >
+                    Start
+                </button>
+            </a>
+
+        </div>
+    )
+}
+
+export default MyLevelPage
