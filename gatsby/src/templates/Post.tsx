d9a6ca7a8bacb8974407560477f5f271d1c46305
Thomas Z Lukoma <dev.tzlukoma@gmail.com>
2023-05-05 17:43:14 -0400

    remove date from blog posts

--- gatsby/src/templates/Post.tsx
+++ gatsby/src/templates/Post.tsx
@@ -9,11 +9,9 @@ export default function SinglePostPage({ data }) {
     const categories = data.post.category
     const author = data?.post?.author?.name
     const title = data.post.title
-    const date = data.post.publishedAt
     return (
         <div className="m-auto p-8 md:px-8 lg:px-20">
             <h1 className="text-4xl text-center">{title}</h1>
-            <h2 className="text-2xl text-center my-0 text-primary-300">{displayLocalTimeZone(date, 'MMM DD, YYYY')}</h2>
             {author ? <h2 className="text-2xl text-center mt-0">{`by ${author}`}</h2> : null}
             <BlockContent blocks={content} className="prose lg:prose-xl m-auto" />
         </div>)
@@ -32,7 +30,6 @@ export const query = graphql`
                 category {
                     name
                 }
-                publishedAt
                 _rawBody(resolveReferences: {maxDepth: 10})
             }
         }
